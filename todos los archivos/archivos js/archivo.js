9b00d496a789fa5d172907962cea4c74aa0d9e1a
lara beron <laraberon1818@gmail.com>
2024-08-12 17:16:51 -0300

    agrego alert

--- todos los archivos/archivos js/archivo.js
+++ todos los archivos/archivos js/archivo.js
@@ -1,6 +1,6 @@
 // pruebo cuadro de diálogo navegador
 
 {
-    let nombre = prompt("¿Cuál es tu nombre?");
+    let nombre = prompt ("¿Cuál es tu nombre?");
     alert("Hola " + nombre + ", bienvenidx a nuestro sitio :) ¡Esperamos que te sea útil y lo disfrutes!");
 }
