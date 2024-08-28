71f7e75881a3a47f5774b52d80d57435f6f6b5c0
Candehaedo11 <candehaedo64@gmail.com>
2024-08-28 16:03:57 -0300

    Agregue test de bienestar

--- todos los archivos/archivos js/archivo.js
+++ todos los archivos/archivos js/archivo.js
@@ -24,3 +24,25 @@ function agregarComentario(nombre, comentario) {
     `;
 	contenedorComentarios.innerHTML += comentarioHTML;
 };
+
+// test de bienestar
+const form = document.getElementById('test-form');
+const resultados = document.getElementById('resultados');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const pregunta1 = document.querySelector('input[name="pregunta1"]:checked').value;
+	const pregunta2 = document.querySelector('input[name="pregunta2"]:checked').value;
+	
+	// Lógica para calcular el resultado del test
+	let resultado = '';
+	if (pregunta1 === 'Bien' && pregunta2 === '5-7 horas') {
+		resultado = 'Tu bienestar es excelente!';
+	} else if (pregunta1 === 'Regular' && pregunta2 === '5-7 horas') {
+		resultado = 'Tu bienestar es bueno, pero podrías mejorar.';
+	} else {
+		resultado = 'Tu bienestar es regular, debes tomar medidas para mejorar.';
+	}
+	
+	resultados.innerHTML = resultado;
+});
