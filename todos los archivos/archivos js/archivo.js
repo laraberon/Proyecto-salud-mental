b5b4ea00831291e0813e8a0e5283020adff9d478
Candehaedo11 <candehaedo64@gmail.com>
2024-08-28 14:49:07 -0300

    Agregue foro de comentarios a pag inicial

--- todos los archivos/archivos js/archivo.js
+++ todos los archivos/archivos js/archivo.js
@@ -4,3 +4,23 @@
     let nombre = prompt ("¿Cuál es tu nombre?");
     alert("Hola " + nombre + ", bienvenidx a nuestro sitio :) ¡Esperamos que te sea útil y lo disfrutes!");
 }
+
+// foro de comentarios
+
+document.getElementById("comentario-form").addEventListener("submit", function(event) {
+	event.preventDefault();
+	var nombre = document.getElementById("nombre").value;
+	var comentario = document.getElementById("comentario").value;
+	agregarComentario(nombre, comentario);
+});
+
+function agregarComentario(nombre, comentario) {
+	var contenedorComentarios = document.getElementById("comentarios");
+	var comentarioHTML = `
+		<div class="comentario">
+			<span class="nombre">${nombre}</span>
+			<span class="texto">${comentario}</span>
+		</div>
+    `;
+	contenedorComentarios.innerHTML += comentarioHTML;
+}
